6e4406193b6c8a9961abe80e0a021d780a75bf2c
Vox1oot <voxfoot@gmail.com>
2023-02-28 23:14:04 +0500

    feat: implement func when take string and generate reverse polish notaion

---
+++ src/utils/generateRPN.ts
@@ -0,0 +1,59 @@
+type OperationsType = {
+    '+': number;
+    '-': number;
+    '*': number;
+    '/': number;
+    '√': number;
+    '%': number;
+};
+
+const genertaeRPN = (input: string): string[] => {
+    if (input.length == 0) {
+        return [];
+    }
+
+    const operators: any = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+        '√': 3,
+        '%': 3,
+    };
+
+    let output: string[] = [];
+    let stack: string[] = [];
+
+    const tokens: string[] | null = input.match(/\d\.{0,1}\d*|[+\-*\/%√]/g); //разбиваем строку на токены
+
+    if (tokens == null) {
+        throw new Error('Unexpected operations');
+    }
+
+    for (let i = 0; i < tokens.length; i += 1) {
+        const token = tokens[i];
+
+        if (/\d+/.test(token)) {
+            output.push(token); //int & float кладем в output
+        } else {
+            while (stack.length > 0 && operators[token] <= operators[stack[stack.length - 1]]) {
+                const operation = stack.pop();
+                if (operation != undefined) {
+                    output.push(operation);
+                }
+            }
+            stack.push(token);
+        }
+    }
+
+    while (stack.length > 0) {
+        const operation = stack.pop();
+        if (operation != undefined) {
+            output.push(operation);
+        }
+    }
+
+    return output;
+};
+
+export default genertaeRPN;
