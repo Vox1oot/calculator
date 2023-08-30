8182f264af5d66f24f0f315533cca058dc4f866c
Vitaliy Morzhov <voxfoot@gmail.com>
2023-08-30 23:54:57 +0500

    refactoring

--- src/utils/generateRPN.ts
+++ src/utils/generateRPN.ts
@@ -1,56 +1,53 @@
-interface Ioperators {
-    [key: string]: number;
-}
+type Operator = '+' | '-' | '*' | '/' | '√' | '%'
 
-const genertaeRPN = (input: string): string[] => {
+/* Приоритеты операторов */
+const operators: Record<Operator, number> = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+    '√': 3,
+    '%': 3,
+};
+
+const generateRPN = (input: string): string[] => {
     if (input.length === 0) {
         return [];
     }
 
-    /* Приоритеты операторов */
-    const operators: Ioperators = {
-        '+': 1,
-        '-': 1,
-        '*': 2,
-        '/': 2,
-        '√': 3,
-        '%': 3,
-    };
-
     const output: string[] = [];
-    const stack: string[] = [];
-
+    const stack: Operator[] = [];
     const tokens: string[] | null = input.match(/\d+\.{0,1}\d*|[+\-*\\/%√]/g); // разбиваем строку на операторы и операнды
+
+    const moveStackToOutput = (stackOperators: Operator[], outputs: string[]) => {
+        const operation = stackOperators.pop();
+
+        if (operation !== undefined) {
+            outputs.push(operation);
+        }
+    };
 
     if (tokens == null) {
         throw new Error('Unexpected operations');
     }
 
-    for (let i = 0; i < tokens.length; i += 1) {
-        const token = tokens[i];
-
+    tokens.forEach((token) => {
         if (/\d+/.test(token)) {
             output.push(token); // операнды int и float кладем в output
         } else {
             while (stack.length > 0
-                && operators[token] <= operators[stack[stack.length - 1]]) {
-                const operation = stack.pop();
-                if (operation !== undefined) {
-                    output.push(operation);
-                }
+                && operators[token as Operator] <= operators[stack[stack.length - 1]]) {
+                moveStackToOutput(stack, output);
             }
-            stack.push(token);
+            stack.push(token as Operator);
         }
-    }
+    });
 
     while (stack.length > 0) {
-        const operation = stack.pop();
-        if (operation !== undefined) {
-            output.push(operation);
-        }
+        moveStackToOutput(stack, output);
     }
 
     return output;
 };
 
-export default genertaeRPN;
+export default generateRPN;
